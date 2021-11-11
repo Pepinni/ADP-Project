9e2e1009b65da661ea08b9da20a705e69ca24b35
gd-codes <45459312+gd-codes@users.noreply.github.com>
2021-11-11 11:09:33 +0530

    Added Execution instructions to Readme & more comments to app.js

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-// Requiring the necessary libraries
+/* 
+------------------------------------------------
+CS207 Applied Databases Practicum
+Project by Group 6 : Campus Returning Scheduler
+Main Code file
+------------------------------------------------
+*/
+
+// Require the necessary libraries
 require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
@@ -33,13 +41,14 @@ var upload = multer({ storage: storage });
 
 
 // Connecting to local 'ADP-Project' datbase
-// mongoose.connect("mongodb://localhost:27017/ADP-Project", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// console.log("Debug : Database ID : ", String(process.env.PASS));
+mongoose.connect("mongodb://localhost:27017/ADP-Project", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log("Debug : Database ID : ", String(process.env.PASS));
 // Use below line in deployment environment, with a specific database connection
-mongoose.connect(String(process.env.PASS),{ useNewUrlParser: true , useUnifiedTopology: true});
+/* mongoose.connect(String(process.env.PASS),
+                    { useNewUrlParser: true , useUnifiedTopology: true}); */
 
 
 // Creating a database schema
@@ -74,10 +83,12 @@ var uName;
 
 
 ///////////////////////////////////////////////////////////////////
-/////////// All the access and redirect routes below////////////////
+/////////// All the access and redirect routes below///////////////
 ///////////////////////////////////////////////////////////////////
 
-app.get("/", function (req, res) { //login
+// HTTP "GET" requests to load a page based on the given path
+// HTTP "POST" requests to submit form data
+app.get("/", function (req, res) {
   res.render("index_login.ejs", {msg:""});
 });
 
@@ -89,41 +100,40 @@ app.get('/reset', function(req,res){
   res.render('reset.ejs', {msg : ""});
 })
 
-
+// Login form data posted here
 app.post("/login", function (req, res) {
   uName = req.body.email;
   var pwd = req.body.password;
   const newUser = new User({
     userName: uName,
     password: pwd,
-    // img : Binary(req.body.file)
   });
   User.find({ userName: uName}, function (err, foundUser) {
     if (err) {
       console.log(err);
     } else {
-      if (foundUser.length == 1) {
-        // res.render("form.ejs");
+      if (foundUser.length == 1) {    // The user exists in the database
+                                      // (Has registered before)
         if (foundUser[0].password === pwd) {
-          console.log("Password matches and he will be logged in");
+          console.log("Password matches - logging in user ", uName);
           if(foundUser[0].fname == ""){
-            res.render("request.ejs");
-          }
+            res.render("request.ejs");    // If the user's data hasnt been saved
+          }                               // Display the form
           else{
             res.render('viewdata.ejs', {data : foundUser[0]});
-          }
+          }                               // Otherwise show the submitted data
           
         } 
         else {
-            console.log(foundUser);
+            console.log(foundUser);       // Wrong password
           console.log("The password is a mismatch");
           res.render("index_login.ejs", {msg:"The password is incorrect!"})
         }
       } 
       else {
-          newUser.save(function(err){
-              if(err){
-                  console.log(err);
+          newUser.save(function(err){ // The user doesn't exist
+              if(err){                // Add the login details to the database
+                  console.log(err);   // And then display the form
               }
               else{
                   console.log("New User created");
@@ -135,13 +145,14 @@ app.post("/login", function (req, res) {
   });
 });
 
+// The return request form data posted here
 app.post('/submit', upload.single('formFile'), function(req, res) {
   User.findOne({userName : uName}, function(err, foundUser){
     if(err){
-      console.log(err);
-    }
+      console.log(err);   // Check for the user's record in the database
+    }                     // It would have been created at login
     else{
-      foundUser.fname = req.body.fname;
+      foundUser.fname = req.body.fname;     // Save the data from the form
       foundUser.lname = req.body.lname;
       foundUser.address = req.body.address;
       foundUser.city = req.body.city;
@@ -154,9 +165,9 @@ app.post('/submit', upload.single('formFile'), function(req, res) {
       foundUser.date2 = req.body.date2;
       foundUser.reason = req.body.reason;
       foundUser.upload = 
-        (req.file.filename != undefined)? req.file.filename : '';
+      (req.file.filename != undefined)? req.file.filename : '';
       foundUser.uploadType = 
-        (req.file.mimetype != undefined)? req.file.mimetype : '';
+      (req.file.mimetype != undefined)? req.file.mimetype : '';
       console.log("Info saved");
       console.log(req.body);
     }
@@ -164,27 +175,27 @@ app.post('/submit', upload.single('formFile'), function(req, res) {
       if(err){
         console.log(err);
       }
-      else{
+      else{     // After storing details, display the saved data page
         console.log("Details are modified successfully");
         res.render('viewdata.ejs', {data : foundUser});
       }
     })
   })  
-  // console.log(req.body);
 });
 
 
+// Initiate a password reset
 var otp;
 app.post("/otp", function(req,res){
-
+  
   studentMail = req.body.email;
-  otp = Math.round(Math.random() * 1000000);
-
+  otp = Math.round(Math.random() * 1000000);  // Generate an OTP
+  
   var msg = `
-    <h3>This is your 6-digit OTP valid only for 10 minutes</h3>
-    <h>${otp}</h>
-  `
-
+  <h3>This is your 6-digit OTP valid only for 10 minutes</h3>
+  <h>${otp}</h>
+  `;                                          // Content of email to be sent
+  
   User.find({userName : studentMail}, function(err, foundUser){
     if(err){
       console.log(err);
@@ -193,31 +204,35 @@ app.post("/otp", function(req,res){
       if(foundUser.length === 1){
         async function main() {
           /* IMPORTANT : Configure the password in the development environment's
-           variables. Authentication will fail otherwise. The file is not tracked in git */
+          variables. Authentication will fail otherwise. The file is not tracked in git */
           // create reusable transporter object using the default SMTP transport
           let transporter = nodemailer.createTransport({
-          host: "smtp-mail.outlook.com",
-          // host: "smtp.mail.yahoo.com",
-          port: 587,
-          secure: false, // true for 465, false for other ports
-          auth: {
+            // host : "smtp.gmail.com",       /* For Gmail sender */
+            // host: "smtp.mail.yahoo.com",  
+            host: "smtp-mail.outlook.com", 
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+              // user: "user@gmail.com",  /* For Gmail sender */
               user: "piyushverma0007@outlook.com", // generated ethereal user
-              // user: "piyushverma476@yahoo.com", // generated ethereal user
               pass: process.env.mailPASS, // generated ethereal password
-          },
-          tls:{
+            },
+            tls:{
               rejectUnauthorized:false
-          },
+            },
           });
-      
+          /* NOTE : For sending from a Gmail account, additionally enable
+          "Less Secure App Access" from Google settings to allow authentication
+          via any script */
+          
           // send mail with defined transport object
           let info = await transporter.sendMail({
-          from: "OAS-IITMandi"+' <piyushverma0007@outlook.com>', // sender address
-          to: studentMail, // list of receivers
-          subject: "Reset", // Subject line
-          html: msg, // html body
+            from: "OAS-IITMandi"+' <piyushverma0007@outlook.com>', // sender address
+            to: studentMail, // list of receivers
+            subject: "Reset", // Subject line
+            html: msg, // html body
           });
-      
+          
           console.log("Message sent: %s", info.messageId);
           console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
           return res.render("reset.ejs", {msg : ""});
@@ -229,8 +244,11 @@ app.post("/otp", function(req,res){
       }
     }
   })
-
+  
 })
+
+
+// The password reset request posted here
 
 app.post("/pwdreset", function(req,res){
   const uName = req.body.email
@@ -265,10 +283,11 @@ app.post("/pwdreset", function(req,res){
     res.render('reset.ejs', {msg: "Invalid OTP"});
   }
 })
+
+
+// Finally, *Start the server*
 // The server will listen on port 3000 on localhost and random port when hosted online
 app.listen(process.env.PORT||3000, function () {
   console.log("Server running on port 3000");
 });
 
-
-// 12QW#$er
